a3d18aa7e6904898e59085199735a17a3036aa76
Are-Jae <evansraven7@gmail.com>
2023-06-07 00:39:18 -0400

    added more to index.js, markdown unedited

--- Develop/index.js
+++ Develop/index.js
@@ -1,48 +1,75 @@
 // TODO: Include packages needed for this application
 
-const inquirer = require ("inquirer");
+const inquirer = require('inquirer');
 
 const fs = require('fs');
 
-const utils = require("util")
 
-const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [
     {
     type: "input",
     prompt: "What is the title of your project?",
-    name: "Title", {
+    name: "Title",
+    },
+     {
         type: "input",
         prompt: "What is your project for? Describe it in detail.",
         name: "Description",
-
-    }, {
-        type: 
-        prompt: 
-        name: 
 
     }, {
         type: "input",
         prompt: "Table of contents",
         name: "Table of contents", 
 
-
-    }, {
+    }, 
+     {
         type: "input",
         prompt: "What are the installation instructions for your project?",
         name: "Installation",
-    }, 
+    }, {
+        type: "input",
+        prompt: "Usage",
+        name: "How will your app be used?", 
+
+    }, {
+        type: "input",
+        prompt: "Licenses",
+        name: "What license, if any, was used for this project?", 
+
+
+
+    }
     
-}
+
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        console.log(`Congrats! README file '${fileName}' created successfully.`);
+      });
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+    .promptUser(questions)
+    .then((answers) => {
+      const markdownContent = generateMarkdown(answers); 
+      const fileName = 'README.md';
+      writeToFile(fileName, markdownContent);
+    })
+    .catch((error) => {
+      console.error('An error occurred:', error);
+    });
+}
 
 // Function call to initialize app
 init();
