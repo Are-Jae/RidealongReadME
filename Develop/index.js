9abfee8d3aea3dc8ff9f23fe91f7d6d9c672263c
Are-Jae <evansraven7@gmail.com>
2023-06-07 00:14:19 -0400

    added some questions to array

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,42 @@
 // TODO: Include packages needed for this application
 
+const inquirer = require ("inquirer");
+
+const fs = require('fs');
+
+const utils = require("util")
+
+const generateMarkdown = require('./utils/generateMarkdown');
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+    type: "input",
+    prompt: "What is the title of your project?",
+    name: "Title", {
+        type: "input",
+        prompt: "What is your project for? Describe it in detail.",
+        name: "Description",
+
+    }, {
+        type: 
+        prompt: 
+        name: 
+
+    }, {
+        type: "input",
+        prompt: "Table of contents",
+        name: "Table of contents", 
+
+
+    }, {
+        type: "input",
+        prompt: "What are the installation instructions for your project?",
+        name: "Installation",
+    }, 
+    
+}
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
--- README.md
+++ README.md
@@ -1,3 +1,12 @@
-# Professional README Generator Starter Code
+##  README Generator 
+This is a node js application
+
+User will answer provided prompts to generate a readME file  
 
 [How to create a Professional README](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
+
+## Index 
+
+- Installation 
+-Usage 
+-License
